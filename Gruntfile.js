a75e0ef88f24c0c095dc70a6199181d7f207c333
Artem Vitiuk <artem@devart.in.ua>
2014-04-23 19:33:40 +0300

    Implement Auth and Apps management

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    grunt.registerTask('test', ['nodeunit']);
-    grunt.registerTask('default', ['jshint', 'test']);
+    grunt.registerTask('test', ['jshint', 'nodeunit']);
+    grunt.registerTask('default', ['test']);
 };
