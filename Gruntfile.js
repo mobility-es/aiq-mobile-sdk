60e62e3f76ace756768032b979c4be58ccd4751d
Alireza <abe@appearnetworks.com>
2014-07-16 15:07:33 +0200

    Added unit test report generation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,13 @@ module.exports = function (grunt) {
         },
 
         nodeunit: {
-            tests: ['test/*_test.js']
+            tests: ['test/*_test.js'], 
+			options: {
+			    reporter: 'junit',
+				reporterOptions: {
+					output: 'test/output'
+				}
+			}
         }
     });
 
